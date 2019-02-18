a41a0731c92171b14063cc527a9ed6e8b40121ff
Bruno Frascino <bfrascino80@gmail.com>
2019-02-17 22:12:55 -0300

    Bug: Removed reference to module that doesn't exist.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,7 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
-  { path: 'bc-reader', loadChildren: './bc-reader/bc-reader.module#BcReaderPageModule' }
+  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' }
 ];
 @NgModule({
   imports: [
