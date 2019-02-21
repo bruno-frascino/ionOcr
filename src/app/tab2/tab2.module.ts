e0a8a26eb635550e6766a6e29c8b92da97e6d8ac
Bruno Mendonça Frascino <brunom@assessorpublico.local>
2019-02-21 16:07:18 -0300

    feat: Included BarcodeScanner provider and button calling scan funcion

--- src/app/tab2/tab2.module.ts
+++ src/app/tab2/tab2.module.ts
@@ -3,7 +3,11 @@ import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
+
 import { Tab2Page } from './tab2.page';
+
 
 @NgModule({
   imports: [
@@ -11,6 +15,9 @@ import { Tab2Page } from './tab2.page';
     CommonModule,
     FormsModule,
     RouterModule.forChild([{ path: '', component: Tab2Page }])
+  ],
+  providers: [
+    BarcodeScanner
   ],
   declarations: [Tab2Page]
 })
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,8 +1,29 @@
 import { Component } from '@angular/core';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {}
+export class Tab2Page {
+
+  constructor(private barcodeScanner: BarcodeScanner){}
+
+  private bCodeData;
+
+  public bScan(){
+    this.barcodeScanner.scan().then(barcodeData => {
+      console.log('Barcode data', barcodeData);
+
+      this.bCodeData = barcodeData;
+      console.log(`bCdata ${this.bCodeData}`);
+
+     }).catch(err => {
+         console.log('Error', err);
+     });
+  }
+
+}
+
+
